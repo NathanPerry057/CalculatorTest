9c800e2e5f8d1db63254c3c5b0fb95dd8f8ba81c
G00394355 <G00394355@atu.ie>
2022-10-19 13:15:40 +0100

    Added an increment test

--- src/main/java/ie/atu/week5/Calculator.java
+++ src/main/java/ie/atu/week5/Calculator.java
@@ -1,4 +1,17 @@
 package ie.atu.week5;
 
 public class Calculator {
+
+    private int countValue;
+
+    public Calculator() {
+        countValue = 0;
+    }
+
+
+    public int increment(){
+
+        countValue++;
+        return countValue;
+    }
 }
